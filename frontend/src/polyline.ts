46bf1f45fa09cf972b90586fc74f924942047dc7
trust <trust4u@hotmail.it>
2021-06-09 09:29:32 +0200

    save

--- frontend/src/polyline.ts
+++ frontend/src/polyline.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { geoPath, json, scaleLinear, select } from "d3";
 import { geoTransform } from "d3-geo";
 import { sliderBottom } from "d3-simple-slider";
-import { polyline } from "leaflet";
+import { LatLng, polyline } from "leaflet";
 import { Data } from "./Data";
 import { myMap } from "./index";
 
@@ -17,6 +17,10 @@ const mypolyline = async () => {
           longitude,
         ]
       );
+    dayLocation;
+    const myPolyline = polyline(dayLocation);
+    myPolyline.addTo(myMap);
+    const geoPolyline = myPolyline.toGeoJSON();
     const svg = select(myMap.getPanes().overlayPane).append("svg");
 
     // if you don't include the leaflet-zoom-hide when a
@@ -24,8 +28,24 @@ const mypolyline = async () => {
     // original SVG
     const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-    const transform = geoTransform({ point: projectPoint });
-    const d3path = geoPath().projection(transform);
+    ((collection) => {
+      // this is not needed right now, but for future we may need
+      // to implement some filtering. This uses the d3 filter function
+      // featuresdata is an array of point objects
+
+      //   const featuresdata = collection.filter(function(d) {
+      //     return d.properties.id == "route1"
+      // })
+      console.log(collection);
+      const transform = geoTransform({
+        point: function (x, y) {
+          const point = myMap.latLngToLayerPoint(new LatLng(y, x));
+          this.stream.point(point.x, point.y);
+        },
+      });
+
+      const d3path = geoPath().projection(transform);
+    })(geoPolyline);
   } catch (err) {
     console.error(err.message);
   }
